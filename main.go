569ac3a90d92b2387c441aad6842f1c78858785a
Michael Faille <michael@faille.io>
2015-09-03 12:23:27 -0400

    Paralize bridge creation

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/mikefaille/docker-group-daemonizer/dockerGrp"
 	"github.com/mikefaille/docker-group-daemonizer/unixGrp"
 	"github.com/mikefaille/tenus"
@@ -12,6 +14,8 @@ type error interface {
 
 func main() {
 
+	var wg sync.WaitGroup
+	wg.Add(1)
 	chanGroup := unixGrp.TakeAllGroups()
 
 	for currentG := range chanGroup {
@@ -19,24 +23,27 @@ func main() {
 		gDocker, err := dockerGrp.CatchDockerEqGroup(currentG)
 
 		if err == nil {
+			wg.Add(1)
+			go func() {
+				gDocker.GenerateDockerDaemon()
 
-			gDocker.GenerateDockerDaemon()
+				if ok, err := tenus.IsInterfaceExist(gDocker.Name); !ok || err != nil {
+					check(err)
+					gDocker.AddNewDockerBr()
 
-			if ok, err := tenus.IsInterfaceExist(gDocker.Name); !ok || err != nil {
-				check(err)
-				gDocker.AddNewDockerBr()
+				} else {
+					err := tenus.DelBridgeWithName(gDocker.Name)
 
-			} else {
-				err := tenus.DelBridgeWithName(gDocker.Name)
-
-				check(err)
-				gDocker.AddNewDockerBr()
-			}
-
+					check(err)
+					gDocker.AddNewDockerBr()
+				}
+				wg.Done()
+			}()
 		}
 
 	}
-
+	wg.Done()
+	wg.Wait()
 }
 
 func check(e error) {
